f27f7a9bbd7f2b01574f3f7de6ab9065d9d90ddc
faramarz <37065807+newlighty@users.noreply.github.com>
2023-07-04 15:41:54 +0330

    change my site title useing custom_context_processor

---
+++ frobshop/frobshop/custom_context_processor.py
@@ -0,0 +1,2 @@
+def site_name(request):
+    return {'site_name': 'mymug'}
--- frobshop/frobshop/settings.py
+++ frobshop/frobshop/settings.py
@@ -126,6 +126,7 @@ TEMPLATES = [
                 'oscar.apps.checkout.context_processors.checkout',
                 'oscar.apps.communication.notifications.context_processors.notifications',
                 'oscar.core.context_processors.metadata',
+                'frobshop.context_processors.custom_context_processor'
 
             ],
         },
